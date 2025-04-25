d97be7dcf461ab027a02442945a5c0c7f1a1d813
Juan Flores <juancfm99@gmail.com>
2025-04-25 18:18:02 -0500

    Edits 24 abril

--- Crypto-IOS/Crypto-IOS/Models/Asset.swift
+++ Crypto-IOS/Crypto-IOS/Models/Asset.swift
@@ -1,32 +1,29 @@
 import Foundation
 
-struct Asset:Decodable, Identifiable, Equatable{
+struct Asset: Decodable, Identifiable, Equatable {
     let id: String
     let name: String
     let symbol: String
     let priceUsd: String
     let changePercent24Hr: String
     
-    
-    
-    /*
-    var percentage:Double{
-        Double(changePercent24hr) ?? 0
-    }
-    
-    var formattedPrice:String{
-        String(format: "%.2f", Double(priceUsd) ?? 0)
-    }
-    
-    var formattedPercentage:String{
-        String(format: "%.2f", Double(changePercent24hr) ?? 0)
-    }
-    
-    var iconUrl:URL?{
-        URL(string: "https://raw.githubusercontent.com/nomics-project/assets/master/icons/16x16/\(symbol).png")
-    }*/
-    
+//    enum CodingKeys: String, CodingKey {
+//        case id
+//        case name
+//        case symbol
+//        case priceUsd = "price_usd"
+//        case changePercent24Hr = "change_percent_24_hr"
+//    }
 }
 
 
-
+//struct Persona: Identifiable {
+//
+//    var id: String {
+//        UUID().uuidString
+//    }
+//
+//    let ci: String
+//    let name: String
+//    let lastname: String
+//}
